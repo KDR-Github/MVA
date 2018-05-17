9567da14a0b66bfcdb4c33bc720f037019600414
KDR Git <krunalrele@gmail.com>
2018-05-17 23:19:16 +0530

    WebAPI Intro P1

---
+++ ASP.NET Core - Intermediate/WebAPISample/WebAPISample/Controllers/TicketController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPISample.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Ticket")]
+    public class TicketController : Controller
+    {
+        private TicketContext _context;
+
+        public TicketController(TicketContext context)
+        {
+            _context = context;
+            
+            if (_context.TicketItems.Count() == 0)
+            {
+                _context.TicketItems.Add(
+                    new TicketItem
+                    {
+                        Concert = "Beyonce Concert",
+                        Artist = "Beyonce",
+                        Available = true
+                    }
+                );
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<TicketItem> GetAll()
+        {
+            return _context.TicketItems.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{Id}", Name ="GetTicket")]
+        public IActionResult GetById(long id)
+        {
+            var ticket = _context.TicketItems.FirstOrDefault(t => t.Id == id);
+            if (ticket == null)
+            {
+                return NotFound(); //404
+            }
+
+            return new ObjectResult(ticket); //200
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]TicketItem ticket)
+        {
+            if(ticket == null)
+            {
+                return BadRequest();
+            }
+
+            _context.TicketItems.Add(ticket);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTicket", new { id = ticket.Id }, ticket);
+        }
+    }
+}
